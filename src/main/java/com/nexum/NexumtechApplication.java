7f8062393a6f57d0380c6029b83026181b2a8dbc
vinicius <viniciusamalia@gmail.com>
2022-11-05 13:00:25 -0300

    inicio nexum tech

---
+++ src/main/java/com/nexum/NexumtechApplication.java
@@ -0,0 +1,13 @@
+package com.nexum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexumtechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NexumtechApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nexum/NexumtechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nexum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NexumtechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
